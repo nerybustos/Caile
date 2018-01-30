d268dac5dfbcf44dd76260c50d4b8ec9b2c7743b
Nery <nery@macbook-pro-de-nery.local>
2018-01-30 12:55:42 -0600

    Archivos faltantes

---
+++ Caile/ContainerViewController.swift
@@ -0,0 +1,115 @@
+//
+//  ContainerViewController.swift
+//
+//
+//  Created by Aaqib Hussain on 03/08/2015.
+//  Copyright (c) 2015 Kode Snippets. All rights reserved.
+//
+
+import UIKit
+
+open class ContainerViewController: UIViewController {
+    
+    //Varioble para colocar el lugar seleccionado
+    var googlePlace = GooglePlace()
+    var googlePlaceDetail = GooglePlaceDetail()
+    
+    
+    //Manipulating container views
+    fileprivate weak var viewController : UIViewController!
+    //Keeping track of containerViews
+    fileprivate var containerViewObjects = Dictionary<String,UIViewController>()
+    /** Pass in a tuple of required TimeInterval with UIViewAnimationOptions */
+    var animationDurationWithOptions:(TimeInterval, UIViewAnimationOptions) = (0,[])
+    
+    /** Specifies which ever container view is on the front */
+    open var currentViewController : UIViewController{
+        get {
+            return self.viewController
+            
+        }
+    }
+    
+    
+    fileprivate var segueIdentifier : String!
+    
+    /*Identifier For First Container SubView*/
+    @IBInspectable internal var firstLinkedSubView : String!
+    
+    
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    open override func viewDidAppear(_ animated: Bool) {
+        if let identifier = firstLinkedSubView{
+            segueIdentifierReceivedFromParent(identifier)
+        }
+    }
+    override open func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func segueIdentifierReceivedFromParent(_ identifier: String){
+        
+        self.segueIdentifier = identifier
+        self.performSegue(withIdentifier: self.segueIdentifier, sender: nil)
+        
+    }
+    
+    
+    
+    
+    override open func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        //Identifica que ViewController se va a lanzar
+        
+        if segue.identifier == "uno"{
+            let vc = segue.destination as! ViewControllerInfo
+            vc.googlePlace = self.googlePlace
+        }else if segue.identifier == "dos"{
+            let vc = segue.destination as! ViewControllerResenas
+            vc.googlePlaceDetalle = self.googlePlaceDetail
+        }else if segue.identifier == "tres"{
+            let vc = segue.destination as! ViewControllerFotos
+            vc.googlePlaceDatail = self.googlePlaceDetail
+        }
+        
+        if segue.identifier == segueIdentifier{
+            //Remove Container View
+            if viewController != nil{
+                viewController.view.removeFromSuperview()
+                viewController = nil
+                
+            }
+            //Add to dictionary if isn't already there
+            if ((self.containerViewObjects[self.segueIdentifier] == nil)){
+                viewController = segue.destination
+                self.containerViewObjects[self.segueIdentifier] = viewController
+                
+            }else{
+                for (key, value) in self.containerViewObjects{
+                    
+                    if key == self.segueIdentifier{
+                        viewController = value
+                    }
+                }
+                
+            }
+            UIView.transition(with: self.view, duration: animationDurationWithOptions.0, options: animationDurationWithOptions.1, animations: {
+                self.addChildViewController(self.viewController)
+                self.viewController.view.frame = CGRect(x: 0,y: 0, width: self.view.frame.width,height: self.view.frame.height)
+                self.view.addSubview(self.viewController.view)
+            }, completion: { (complete) in
+                self.viewController.didMove(toParentViewController: self)
+            })
+           
+           
+            
+        }
+        
+    }
+    
+    
+}
